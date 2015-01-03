2fc96b79a2e30ff125b3fe51117dab0adf60df9b
Mike Luby <Michael@mLuby.com>
2015-01-03 12:42:30 -0500

    refactor to Value and log tasks button

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var app = angular.module('memory', []);
+var app = angular.module('Memory', []);
 
-app.controller('memoryController', function($scope){
-  $scope.tasks = [
+app.value('Tasks', [
       {title:'laundry',   completed:false}
      ,{title:'dishes',    completed:true}
      ,{title:'groceries', completed:false}
-  ]
+]);
+
+app.controller('MemoryController', function($scope, Tasks){
+  $scope.tasks = Tasks
+  $scope.listTasks = function(){
+    console.log(Tasks);
+  }
 });
 
-app.controller('taskController', function($scope){
+app.controller('TaskController', function($scope){
   $scope.toggleCompletion = function(){
     this.task.completed = !this.task.completed;
   }
