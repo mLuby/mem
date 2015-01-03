fb88c05233a6d1317efe11066f81f808b9cea8bc
Mike Luby <Michael@mLuby.com>
2015-01-03 12:32:52 -0500

    tasks togglable

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-# angular?
+var app = angular.module('memory', []);
+
+app.controller('memoryController', function($scope){
+  $scope.tasks = [
+      {title:'laundry',   completed:false}
+     ,{title:'dishes',    completed:true}
+     ,{title:'groceries', completed:false}
+  ]
+});
+
+app.controller('taskController', function($scope){
+  $scope.toggleCompletion = function(){
+    this.task.completed = !this.task.completed;
+  }
+});
