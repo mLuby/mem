c973ee8071f6f2e4fad0da36734c4b1792c45d4c
Mike Luby <Michael@mLuby.com>
2015-01-27 23:40:01 -0800

    add command line functionality

--- node/Memory.js
+++ node/Memory.js
@@ -1,3 +1,24 @@
 var storage = require('./Storage.js');
 
-console.log(storage.get);
+var command = process.argv[2];
+/*
+add
+list
+done
+edit
+delete
+*/
+var args = process.argv.slice(3);
+// 'task name' 'due by' 'duration'
+// console.log('args', args);
+if(command==='add'){
+  storage.add(args);
+}
+if(command==='done'){
+  storage.done(args[0]);
+}
+if(command==='list'){
+  storage.list().forEach(function(task){
+    console.log(typeof task === 'object' ? JSON.parse(task) : task);
+  });
+}
--- node/Storage.js
+++ node/Storage.js
@@ -8,14 +8,33 @@ if (typeof localStorage === "undefined" || localStorage === null) {
   localStorage = new LocalStorage('./.storage');
 }
 
+var createTask = function(name, dueBy, duration){
+  localStorage.setItem(name, JSON.stringify({name: name, dueBy: dueBy, duration: duration}));
+};
 
-localStorage.setItem('myFirstKey', 'myFirstValue');
-console.log(localStorage.getItem('myFirstKey'));
+var completeTask = function(key){
+  var task = localStorage.getItem(key);
+  console.log('task',task);
+  task.status = 'done';
+  localStorage.setItem(key, JSON.stringify(task));
+};
 
+var list = function(){
+  var results = [];
+  for(var i=0; i<localStorage.length; i++){
+    results.push(
+        localStorage.getItem(
+          localStorage.key(i)
+        )
+    );
+  }
+  return results;
+};
 
 module.exports = {
-  add: create,
-  get: localStorage.get('myFirstKey'),
-  edit: update,
-  delete: destroy
+  add: createTask,
+  edit: createTask,
+  done: completeTask,
+  delete: localStorage.removeItem,
+  list: list
 };
