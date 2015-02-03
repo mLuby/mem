899d250dc1a7979bfca197226bcb94f5b7a6ee3e
Mike Luby <Michael@mLuby.com>
2015-02-03 00:46:11 -0800

    reorganize file structure

--- angular/Memory.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('Memory', ['LocalStorage']);
-
-app.value('tasks', [
-    {name:'Buy milk', duration:'30m', dueBy:'Tomorrow', blocking:'4h'}
-    ,{name:'Fix watch', duration:'1d', dueBy:'Next week', blocking:'10m'}
-    ,{name:'Meet Tom', duration:'1.5h', dueBy:'Today', blocking:''}
-  ]);
--- angular/README.md
+++
@@ -1,4 +0,0 @@
-Memory
-======
-
-It helps you remember things, free up brain RAM, and get things done.
